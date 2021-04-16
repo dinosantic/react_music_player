0824659df2f5addae1935e545feb285b550e3760
Dino Santic <dino@everest.ba>
2021-04-16 10:47:49 +0200

    player update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
 import data from "./utils";
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
